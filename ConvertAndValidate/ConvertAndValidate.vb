39772fa6b5a90a427f4c25ae0cfd0e1984c99d7b
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-21 20:34:49 -0600

    Add variables and function.

--- ConvertAndValidate/ConvertAndValidate.vb
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -2,12 +2,22 @@
 'RCET0265
 'Spring 2021
 'ConvertAndValidate
-
+'https://github.com/heinelli/ConvertAndValidate.git
 
 Module ConvertAndValidate
-
     Sub Main()
+        Dim convertThisString As String
+        Dim thisInteger As Integer
+
+        convertThisString = CInt(Console.ReadLine())
+        ConvertAndValidate(convertThisString, thisInteger)
+
+
 
     End Sub
+
+    Private Function ConvertAndValidate(ByVal convertThisString As Integer, ByRef toThisInteger As Integer)
+
+    End Function
 
 End Module
