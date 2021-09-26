38b4106f30ef880a9ee811e1644bbd535a5bfc0f
Elliot Heiner <elliotheiner1997@gmail.com>
2021-09-26 17:26:51 -0600

    Add comments and remove extra spacing.

--- ConvertAndValidate/ConvertAndValidate.vb
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -12,8 +12,11 @@ Module ConvertAndValidate
         Dim convertThisString As String
         Dim thisInteger As Integer
 
+        'User input
         Console.WriteLine("Type in a number please.")
-        convertThisString = (Console.ReadLine())
+        convertThisString = Console.ReadLine()
+
+        'Call conversion function and write return value.
         ConvertAndValidate(convertThisString, thisInteger)
         Console.WriteLine(ConvertAndValidate(convertThisString, thisInteger))
         Console.Read()
@@ -22,6 +25,7 @@ Module ConvertAndValidate
     Private Function ConvertAndValidate(ByVal convertThisString As String, ByRef toThisInteger As Integer) As String
         Dim returnMessage As String
 
+        'Convert the input string into an integer value. Catch incorrect values.
         Try
             toThisInteger = CInt(convertThisString)
             returnMessage = ""
